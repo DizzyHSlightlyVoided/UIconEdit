62013165af68e1aafe7022daa50b623b02821299
KimikoMuffin <kimiko.muffin@gmail.com>
2015-06-19 12:33:19 -0400

    Initial version, with a couple of things working already.

---
+++ UIconEdit/IconFileBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace UIconEdit
+{
+    /// <summary>
+    /// Base class for icon and cursor files.
+    /// </summary>
+    public abstract class IconFileBase
+    {
+        const int bmpHeaderSize = 14;
+
+        private void WriteImage(BinaryWriter writer, IIconFrame frame, ref int offset, out Stream writeStream)
+        {
+            var image = frame.Image;
+            bool needPng;
+            if (needPng = (image.Width > byte.MaxValue)) writer.Write(byte.MinValue);
+            else writer.Write((byte)image.Width); //1
+            if (needPng |= (image.Height > byte.MaxValue)) writer.Write(byte.MinValue);
+            else writer.Write((byte)image.Height); //2
+
+            if (needPng |= (image.Palette == null || image.Palette.Entries.Length == 0))
+                writer.Write(byte.MinValue);
+            else
+                writer.Write(image.Palette.Entries.Length); //3
+
+            writer.Write(byte.MinValue); //4
+
+            writer.Write(frame.X); //6
+            writer.Write(frame.Y); //8
+            int length;
+
+            MemoryStream ms = new MemoryStream();
+            if (needPng)
+            {
+                image.Save(ms, ImageFormat.Png);
+                writeStream = ms;
+            }
+            else
+            {
+                image.Save(ms, ImageFormat.Bmp);
+                writeStream = new OffsetStream(ms, bmpHeaderSize, (int)ms.Length, false);
+            }
+            length = (int)writeStream.Length;
+            writer.Write(length); //12
+            writer.Write(offset); //16
+            offset += length;
+        }
+    }
+
+    /// <summary>
+    /// Interface for base classes.
+    /// </summary>
+    public interface IIconFrame
+    {
+        /// <summary>
+        /// Gets and sets the image associated with the current instance.
+        /// </summary>
+        Image Image { get; set; }
+
+        /// <summary>
+        /// Gets the 16-bit X component.
+        /// In ICO format, specifies the color panes; in CUR format, specifies the horizontal offset in pixels of the hotspot from the left.
+        /// </summary>
+        short X { get; }
+        /// <summary>
+        /// Gets the 16-bit Y component.
+        /// In ICO format, specifies bits per pixel; in CUR format, specifies the vertical offset in pixels of the hotspot from the top.
+        /// </summary>
+        short Y { get; }
+    }
+}
---
+++ UIconEdit/OffsetStream.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UIconEdit
+{
+    internal class OffsetStream : Stream
+    {
+        private bool _leaveOpen;
+        private Stream _stream;
+
+        private int _offset, _length, _remainingLength;
+
+        internal OffsetStream(Stream stream, int offset, int length, bool leaveOpen)
+        {
+            _stream = stream;
+            _offset = offset;
+            _leaveOpen = leaveOpen;
+            try
+            {
+                _length = _remainingLength = (int)Math.Min(length, stream.Length - _offset);
+            }
+            catch
+            {
+                _length = _remainingLength = length;
+            }
+        }
+
+        public override bool CanRead
+        {
+            get { return _stream != null; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override long Length
+        {
+            get { return _length; }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                if (_stream == null) throw new ObjectDisposedException(null);
+                return _stream.Position - _offset;
+            }
+            set { throw new NotSupportedException(); }
+        }
+
+        public override void Flush()
+        {
+        }
+
+        public override int Read(byte[] array, int offset, int count)
+        {
+            if (_stream == null) throw new ObjectDisposedException(null);
+            if (array == null) throw new ArgumentNullException("buffer");
+            new ArraySegment<byte>(array, offset, count);
+
+            int readBytes = _stream.Read(array, offset, Math.Min(count, _remainingLength));
+            if (readBytes == 0)
+                _remainingLength = 0;
+            else
+                _remainingLength -= readBytes;
+            return readBytes;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing && !_leaveOpen)
+                    _stream.Dispose();
+                _stream = null;
+                _leaveOpen = true;
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+        }
+
+        public void CopyTo(BinaryWriter writer, int bufferSize)
+        {
+            byte[] buffer = new byte[bufferSize];
+
+            int read = Read(buffer, 0, bufferSize);
+
+            while (read != 0)
+            {
+                writer.Write(buffer, 0, read);
+                read = Read(buffer, 0, bufferSize);
+            }
+        }
+
+        public void CopyTo(BinaryWriter writer)
+        {
+            CopyTo(writer, 8192);
+        }
+    }
+}

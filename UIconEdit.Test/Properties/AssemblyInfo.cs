bbb4039d785304d3bace225855e0aef60ae631cf
KimikoMuffin <kimiko.muffin@gmail.com>
2015-06-28 17:54:25 -0400

    Raised the version number due to the preceding Big Change ...

--- UIconEdit.Test/Properties/AssemblyInfo.cs
+++ UIconEdit.Test/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UIconBuilder")]
+[assembly: AssemblyTitle("UIconEdit.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UIconBuilder")]
+[assembly: AssemblyProduct("UIconEdit.Test")]
 [assembly: AssemblyCopyright("Copyright © 2015 by KimikoMuffin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -29,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]

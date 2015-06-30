4971b3a665869baf8b55ce023df126e34013fb22
KimikoMuffin <kimiko.muffin@gmail.com>
2015-06-30 11:26:34 -0400

    Removed System.Drawing from Builder.

--- UIconEdit.Builder/Program.cs
+++ UIconEdit.Builder/Program.cs
@@ -30,7 +30,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.IO;
 using System.Globalization;

9a034fe84bea2f27ef8698e5526a0f77bd989edc
KimikoMuffin <kimiko.muffin@gmail.com>
2015-08-10 14:41:49 -0400

    Added a UAC shield icon to SettingsWindow for the add-to-context-menu checkbox.

---
+++ UIconEdit/UacShieldFuncs.cs
@@ -0,0 +1,203 @@
+﻿#region BSD License
+/*
+Copyright © 2015, KimikoMuffin.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+3. The names of its contributors may not be used to endorse or promote 
+   products derived from this software without specific prior written 
+   permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion
+
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace UIconEdit.Maker
+{
+    internal static class UacShieldFuncs
+    {
+        internal const int MAX_PATH = 260;
+
+        [DllImport("Shell32.dll", SetLastError = false)]
+        public static extern int SHGetStockIconInfo(SHSTOCKICONID siid, SHGSI uFlags, ref SHSTOCKICONINFO psii);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool DestroyIcon(IntPtr hwnd);
+
+        private static BitmapSource _uacIcon;
+        public static ImageSource UACIcon
+        {
+            get
+            {
+                if (Environment.OSVersion.Version.Major < 6) return null;
+
+                if (_uacIcon == null)
+                {
+                    SHSTOCKICONINFO sii = new SHSTOCKICONINFO();
+                    sii.cbSize = (uint)Marshal.SizeOf(typeof(SHSTOCKICONINFO));
+                    try
+                    {
+                        if (SHGetStockIconInfo(SHSTOCKICONID.SIID_SHIELD, SHGSI.SHGSI_ICON | SHGSI.SHGSI_SMALLICON, ref sii) != 0)
+                            throw new Win32Exception();
+
+                        _uacIcon = Imaging.CreateBitmapSourceFromHIcon(sii.hIcon, new Int32Rect(), BitmapSizeOptions.FromEmptyOptions());
+                        DestroyIcon(sii.hIcon);
+                    }
+                    catch
+                    {
+                    }
+
+                }
+
+                return _uacIcon;
+            }
+        }
+    }
+
+    internal enum SHSTOCKICONID : uint
+    {
+        SIID_DOCNOASSOC = 0,
+        SIID_DOCASSOC = 1,
+        SIID_APPLICATION = 2,
+        SIID_FOLDER = 3,
+        SIID_FOLDEROPEN = 4,
+        SIID_DRIVE525 = 5,
+        SIID_DRIVE35 = 6,
+        SIID_DRIVEREMOVE = 7,
+        SIID_DRIVEFIXED = 8,
+        SIID_DRIVENET = 9,
+        SIID_DRIVENETDISABLED = 10,
+        SIID_DRIVECD = 11,
+        SIID_DRIVERAM = 12,
+        SIID_WORLD = 13,
+        SIID_SERVER = 15,
+        SIID_PRINTER = 16,
+        SIID_MYNETWORK = 17,
+        SIID_FIND = 22,
+        SIID_HELP = 23,
+        SIID_SHARE = 28,
+        SIID_LINK = 29,
+        SIID_SLOWFILE = 30,
+        SIID_RECYCLER = 31,
+        SIID_RECYCLERFULL = 32,
+        SIID_MEDIACDAUDIO = 40,
+        SIID_LOCK = 47,
+        SIID_AUTOLIST = 49,
+        SIID_PRINTERNET = 50,
+        SIID_SERVERSHARE = 51,
+        SIID_PRINTERFAX = 52,
+        SIID_PRINTERFAXNET = 53,
+        SIID_PRINTERFILE = 54,
+        SIID_STACK = 55,
+        SIID_MEDIASVCD = 56,
+        SIID_STUFFEDFOLDER = 57,
+        SIID_DRIVEUNKNOWN = 58,
+        SIID_DRIVEDVD = 59,
+        SIID_MEDIADVD = 60,
+        SIID_MEDIADVDRAM = 61,
+        SIID_MEDIADVDRW = 62,
+        SIID_MEDIADVDR = 63,
+        SIID_MEDIADVDROM = 64,
+        SIID_MEDIACDAUDIOPLUS = 65,
+        SIID_MEDIACDRW = 66,
+        SIID_MEDIACDR = 67,
+        SIID_MEDIACDBURN = 68,
+        SIID_MEDIABLANKCD = 69,
+        SIID_MEDIACDROM = 70,
+        SIID_AUDIOFILES = 71,
+        SIID_IMAGEFILES = 72,
+        SIID_VIDEOFILES = 73,
+        SIID_MIXEDFILES = 74,
+        SIID_FOLDERBACK = 75,
+        SIID_FOLDERFRONT = 76,
+        SIID_SHIELD = 77,
+        SIID_WARNING = 78,
+        SIID_INFO = 79,
+        SIID_ERROR = 80,
+        SIID_KEY = 81,
+        SIID_SOFTWARE = 82,
+        SIID_RENAME = 83,
+        SIID_DELETE = 84,
+        SIID_MEDIAAUDIODVD = 85,
+        SIID_MEDIAMOVIEDVD = 86,
+        SIID_MEDIAENHANCEDCD = 87,
+        SIID_MEDIAENHANCEDDVD = 88,
+        SIID_MEDIAHDDVD = 89,
+        SIID_MEDIABLURAY = 90,
+        SIID_MEDIAVCD = 91,
+        SIID_MEDIADVDPLUSR = 92,
+        SIID_MEDIADVDPLUSRW = 93,
+        SIID_DESKTOPPC = 94,
+        SIID_MOBILEPC = 95,
+        SIID_USERS = 96,
+        SIID_MEDIASMARTMEDIA = 97,
+        SIID_MEDIACOMPACTFLASH = 98,
+        SIID_DEVICECELLPHONE = 99,
+        SIID_DEVICECAMERA = 100,
+        SIID_DEVICEVIDEOCAMERA = 101,
+        SIID_DEVICEAUDIOPLAYER = 102,
+        SIID_NETWORKCONNECT = 103,
+        SIID_INTERNET = 104,
+        SIID_ZIPFILE = 105,
+        SIID_SETTINGS = 106,
+        SIID_DRIVEHDDVD = 132,
+        SIID_DRIVEBD = 133,
+        SIID_MEDIAHDDVDROM = 134,
+        SIID_MEDIAHDDVDR = 135,
+        SIID_MEDIAHDDVDRAM = 136,
+        SIID_MEDIABDROM = 137,
+        SIID_MEDIABDR = 138,
+        SIID_MEDIABDRE = 139,
+        SIID_CLUSTEREDDRIVE = 140,
+        SIID_MAX_ICONS = 175
+
+    }
+
+    [Flags]
+    public enum SHGSI : uint
+    {
+        SHGSI_ICONLOCATION = 0,
+        SHGSI_ICON = 0x000000100,
+        SHGSI_SYSICONINDEX = 0x000004000,
+        SHGSI_LINKOVERLAY = 0x000008000,
+        SHGSI_SELECTED = 0x000010000,
+        SHGSI_LARGEICON = 0x000000000,
+        SHGSI_SMALLICON = 0x000000001,
+        SHGSI_SHELLICONSIZE = 0x000000004
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    internal struct SHSTOCKICONINFO
+    {
+        public uint cbSize;
+        public IntPtr hIcon;
+        public int iSysIconIndex;
+        public int iIcon;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = UacShieldFuncs.MAX_PATH)]
+        public string szPath;
+    }
+}

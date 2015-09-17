b12417bad1399782dfd9bd38a993fa79e2bcc3e2
KimikoMuffin <kimiko.muffin@gmail.com>
2015-09-17 18:26:03 -0400

    Whoops an even better way of doing IList.Add()

--- UIconEdit.Core/IconFileBase.cs
+++ UIconEdit.Core/IconFileBase.cs
@@ -1405,7 +1405,7 @@ namespace UIconEdit
             int IList.Add(object value)
             {
                 if (Add(_checkAdd(value, "value"))) return _items.Count - 1;
-                throw new NotSupportedException("Could not add the specified value!");
+                return -1;
             }
 
 #if DRAWING
